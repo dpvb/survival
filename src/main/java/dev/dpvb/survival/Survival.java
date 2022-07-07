169b7080bbfe9c7f58d8ba2001a1ab5b336f3be9
Dylan van Bunnik <dylan@lawn-143-215-81-145.lawn.gatech.edu>
2022-07-07 19:56:12 -0400

    First commit.

---
+++ src/main/java/dev/dpvb/survival/Survival.java
@@ -0,0 +1,17 @@
+package dev.dpvb.survival;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Survival extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
