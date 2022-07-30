3c88cdd13ea6a87657e2ab6be8143c5babdd89a6
dpvb <bungohdev@gmail.com>
2022-07-29 23:29:31 -0400

    Create simple method of getting items from LootChests.

--- src/main/java/dev/dpvb/survival/chests/LootChestInventory.java
+++ src/main/java/dev/dpvb/survival/chests/LootChestInventory.java
@@ -2,9 +2,12 @@ package dev.dpvb.survival.chests;
 
 import dev.dpvb.survival.gui.InventoryWrapper;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Lidded;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.Inventory;
@@ -15,21 +18,34 @@ import java.util.Random;
 public class LootChestInventory extends InventoryWrapper {
 
     private LootChest chest;
+    private int size;
 
     public LootChestInventory(LootChest chest) {
         this.chest = chest;
+        this.size = 9;
     }
 
     @Override
     protected Inventory generateInventory() {
         Inventory inventory = Bukkit.createInventory(null, 9);
-        inventory.setItem(new Random().nextInt(9), new ItemStack(Material.STICK));
+        inventory.setItem(new Random().nextInt(size), new ItemStack(Material.STICK));
         return inventory;
     }
 
     @Override
     public void handle(InventoryClickEvent event) {
+        if (event.getClickedInventory() == getInventory()) {
+            ItemStack clickedItem = event.getCurrentItem();
+            if (clickedItem == null) {
+                return;
+            }
 
+            getInventory().setItem(event.getSlot(), null);
+            givePlayerItem(event.getWhoClicked(), clickedItem);
+            return;
+        }
+
+        event.setCancelled(true);
     }
 
     @Override
@@ -40,6 +56,13 @@ public class LootChestInventory extends InventoryWrapper {
             if (state instanceof Lidded lidded) {
                 lidded.close();
             }
+        }
+    }
+
+    private void givePlayerItem(HumanEntity player, ItemStack item) {
+        if (!player.getInventory().addItem(item).isEmpty()) {
+            Location l = player.getLocation();
+            l.getWorld().dropItemNaturally(l, item);
         }
     }
 }
