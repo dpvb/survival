6a883d6842d88f0aa17bb5f0e48278fd046be008
dpvb <bungohdev@gmail.com>
2022-08-05 11:30:44 -0400

    Added TNT functionality.

--- src/main/java/dev/dpvb/survival/game/GameListener.java
+++ src/main/java/dev/dpvb/survival/game/GameListener.java
@@ -1,8 +1,13 @@
 package dev.dpvb.survival.game;
 
 import dev.dpvb.survival.stats.PlayerInfoManager;
+import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
@@ -52,6 +57,25 @@ public class GameListener implements Listener {
             manager.remove(player);
             event.setRespawnLocation(manager.hubWorld.getSpawnLocation());
         }
+    }
+
+    @EventHandler
+    public void onTntPlace(BlockPlaceEvent event) {
+        final var player = event.getPlayer();
+        if (!manager.playerInGame(player)) return;
+        if (event.getBlockPlaced().getType() == Material.TNT) {
+            event.getBlock().setType(Material.AIR);
+            // spawn tnt
+            Entity tnt = manager.getArenaWorld().spawn(event.getBlock().getLocation(), TNTPrimed.class);
+            ((TNTPrimed) tnt).setFuseTicks(50);
+        }
+    }
+
+    @EventHandler
+    public void onTntExplode(EntityExplodeEvent event) {
+        if (!(event.getEntity() instanceof TNTPrimed)) return;
+        // Make it so no blocks are destroyed
+        event.blockList().clear();
     }
 
 }
