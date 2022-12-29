f563fc8fe53c1c9ab7af64d615cd30bc3d35e712
dpvb <bungohdev@gmail.com>
2022-12-28 22:17:06 -0500

    Changed calculate function and removed fuzz.

--- src/main/java/dev/dpvb/survive/game/airdrop/NextAirdropCalculator.java
+++ src/main/java/dev/dpvb/survive/game/airdrop/NextAirdropCalculator.java
@@ -19,7 +19,6 @@ public final class NextAirdropCalculator {
     final int ceil;
     final int floor;
     final double scale;
-    final int fuzz;
 
     /**
      * Creates a new calculator.
@@ -27,38 +26,32 @@ public final class NextAirdropCalculator {
      * @param ceil the maximum average time to the next drop
      * @param floor the minimum average time to the next drop
      * @param scale how rapidly the average time decreases with player count
-     * @param fuzz by how much to randomize the time (must be {@code > 0})
      * @throws IllegalArgumentException if {@code fuzz < 0},
      * {@code ceil <= floor} or {@code floor <= fuzz}
      */
-    NextAirdropCalculator(int ceil, int floor, double scale, int fuzz) throws IllegalArgumentException {
-        if (fuzz < 0) {
-            throw new IllegalArgumentException("fuzz must be greater than or equal to 0");
-        }
+    NextAirdropCalculator(int ceil, int floor, double scale) throws IllegalArgumentException {
         if (ceil <= floor) {
             throw new IllegalArgumentException("ceil must be greater than floor");
         }
-        if (floor <= fuzz) {
-            throw new IllegalArgumentException("floor must be greater than fuzz");
+        if (floor <= 0) {
+            throw new IllegalArgumentException("floor must be greater than 0");
         }
         this.ceil = ceil;
         this.floor = floor;
         this.scale = scale;
-        this.fuzz = fuzz;
     }
 
     /**
      * Loads a calculator from a config section.
      *
      * @param section a config section
-     * @see #NextAirdropCalculator(int, int, double, int)
+     * @see #NextAirdropCalculator(int, int, double)
      */
     NextAirdropCalculator(@NotNull ConfigurationSection section) throws IllegalArgumentException {
         this(
                 section.getInt("ceil"),
                 section.getInt("floor"),
-                section.getDouble("scale"),
-                section.getInt("fuzz")
+                section.getDouble("scale")
         );
     }
 
@@ -69,6 +62,6 @@ public final class NextAirdropCalculator {
      * @return the next time-till-the-next-airdrop in minutes
      */
     public int calculate(int playerCount) {
-        return max((int) (ceil - pow(scale, playerCount - 1) - 1), floor) * ThreadLocalRandom.current().nextInt(-fuzz, fuzz) + 1;
+        return (int) (ThreadLocalRandom.current().nextInt(ceil - floor) + floor - playerCount * scale);
     }
 }
